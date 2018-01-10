4cfdde2c8fc8aff7158bae72e6fa57d7383c427c
Jesse Stuart <hi@jessestuart.com>
2018-01-10 17:28:57 -0500

    [alias] Separate out vagrant aliases.

---
+++ zsh/vagrant/aliases.zsh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+
+# ================
+# Vagrant aliases.
+# ================
+alias vag="vagrant"
+alias vagd="vagrant destroy -f"
+alias vagst="vagrant status"
+alias vd="vagrant destroy -f"
+alias vdup="vagrant destroy && vagrant up"
+alias viav="$EDITOR Vagrantfile"
+alias vivag="$EDITOR Vagrantfile"
+alias vp="vagrant provision"
+alias vsh="vagrant ssh"
+alias vssh="vagrant ssh"
+alias vup="vagrant up"
