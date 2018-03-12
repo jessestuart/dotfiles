6e6a243944494b7a2e4edb0f4ff4e8fc9f5198ea
Jesse Stuart <hi@jessestuart.com>
2018-03-12 08:47:25 +0000

    [deb] Update path for Go on pik8s.

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env zsh
 
 # Add the bins.
 export PATH="$HOME/bin:$PATH"
@@ -18,10 +18,12 @@ export PATH=$PATH:$MONGO_PATH/bin
 
 # Python.
 export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+# export PATH="/Users/jestuart/bin/miniconda3/bin:$PATH"
 
 # Yarn / NPM.
 export PATH="/usr/local/opt/yarn/bin:$PATH"
 export PATH="$HOME/.npm-global/bin:$PATH"
+export PATH="/usr/local/node/bin:$PATH"
 
 # Rust.
 export PATH="$PATH:$HOME/.cargo/bin"
@@ -30,22 +32,19 @@ export PATH="$PATH:~/dotfiles/**/*.sh"
 
 export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
 
-# export PATH="/Users/jestuart/bin/miniconda3/bin:$PATH"
-
 export PATH="/usr/local/opt/make/libexec/gnubin:$PATH"
 
 export PATH="$HOME/github/emacs/src:$PATH"
 export PATH="$HOME/.emacs.d/bin:$PATH"
 
-if [ $(uname -s) = 'GNU/Linux' ]; then
+if [ "$(uname -s)" = "GNU/Linux" ] || [ "$(uname -s)" = "Linux" ] ; then
   export GOPATH=$HOME/go
   export GOROOT=/usr/local/go
-  export PATH=$PATH:/usr/local/go/bin:~/go/bin
+  export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
   export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
   export PATH=$PATH/var/home/linuxbrew/.linuxbrew/bin
 fi
 
-# export PATH="$HOME/dev/heketi-client/bin:$PATH"
 export PATH="$HOME/bin/heketi-client/bin:$PATH"
 
 # =============
