8ad8a397b8ca5f4887bacdf3960ff3f5342b1297
Jesse Stuart <hi@jessestuart.com>
2019-12-22 19:25:32 +0000

    feat(dotfiles): Improve systemd logging aliases.
    Signed-off-by: Jesse Stuart <hi@jessestuart.com>

--- zsh/systemd/aliases.zsh
+++ zsh/systemd/aliases.zsh
@@ -11,11 +11,14 @@ alias je="journalctl -er | nvim -R -c 'set ft=messages'"
 # e.g., `jfu kubelet` will show all journal logs for the `kubelet` process(es).
 alias jfu="sudo journalctl -ef -u"
 
-if hash ccze &>/dev/null; then
-  alias jf="sudo journalctl -ef | ccze -A"
-else
-  alias jf="sudo journalctl -ef"
-fi
+function jf() {
+  local lines=$(if [[ -z "$1" ]]; then echo 100; else echo $1; fi)
+  if hash ccze &>/dev/null; then
+    sudo journalctl -ef --lines $lines | ccze -A
+  else
+    sudo journalctl -ef --lines $lines
+  fi
+}
 
 # Nifty aliases for interacting with `systemctl` -- these save me *SO* much time.
 alias stl="sudo systemctl"
