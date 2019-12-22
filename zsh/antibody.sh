ca973acb36ad0126e31700ae0bfdffef48775b54
Jesse Stuart <hi@jessestuart.com>
2019-12-22 19:39:33 +0000

    fix(zsh): Fix antibody.sh generation.
    Signed-off-by: Jesse Stuart <hi@jessestuart.com>

--- zsh/antibody.sh
+++ zsh/antibody.sh
@@ -1,21 +1,21 @@
-source /Users/jesse/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-andrewferrier-SLASH-fzf-z/fzf-z.plugin.zsh
-fpath+=( /Users/jesse/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-andrewferrier-SLASH-fzf-z )
-fpath+=( /Users/jesse/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-greymd-SLASH-tmux-xpanes/completion/zsh )
-source /Users/jesse/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-jessestuart-SLASH-kube-aliases/kube-aliases.plugin.zsh
-fpath+=( /Users/jesse/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-jessestuart-SLASH-kube-aliases )
-fpath+=( /Users/jesse/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-larkery-SLASH-zsh-histdb )
-source /Users/jesse/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async/async.plugin.zsh
-fpath+=( /Users/jesse/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async )
-source /Users/jesse/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
-fpath+=( /Users/jesse/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
-fpath+=( /Users/jesse/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/src )
-source /Users/jesse/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure/pure.plugin.zsh
-fpath+=( /Users/jesse/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure )
-source /Users/jesse/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
-fpath+=( /Users/jesse/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting )
-source /Users/jesse/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
-fpath+=( /Users/jesse/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
-source /Users/jesse/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-jessestuart-SLASH-gtm-terminal-plugin/gtm-plugin.sh
-fpath+=( /Users/jesse/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-jessestuart-SLASH-gtm-terminal-plugin )
-source /Users/jesse/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sobolevn-SLASH-wakatime-zsh-plugin/wakatime.plugin.zsh
-fpath+=( /Users/jesse/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sobolevn-SLASH-wakatime-zsh-plugin )
+source ${ANTIBODY_HOME}/https-COLON--SLASH--SLASH-github.com-SLASH-andrewferrier-SLASH-fzf-z/fzf-z.plugin.zsh
+fpath+=( ${ANTIBODY_HOME}/https-COLON--SLASH--SLASH-github.com-SLASH-andrewferrier-SLASH-fzf-z )
+fpath+=( ${ANTIBODY_HOME}/https-COLON--SLASH--SLASH-github.com-SLASH-greymd-SLASH-tmux-xpanes/completion/zsh )
+source ${ANTIBODY_HOME}/https-COLON--SLASH--SLASH-github.com-SLASH-jessestuart-SLASH-kube-aliases/kube-aliases.plugin.zsh
+fpath+=( ${ANTIBODY_HOME}/https-COLON--SLASH--SLASH-github.com-SLASH-jessestuart-SLASH-kube-aliases )
+fpath+=( ${ANTIBODY_HOME}/https-COLON--SLASH--SLASH-github.com-SLASH-larkery-SLASH-zsh-histdb )
+source ${ANTIBODY_HOME}/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async/async.plugin.zsh
+fpath+=( ${ANTIBODY_HOME}/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async )
+source ${ANTIBODY_HOME}/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( ${ANTIBODY_HOME}/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+fpath+=( ${ANTIBODY_HOME}/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/src )
+source ${ANTIBODY_HOME}/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure/pure.plugin.zsh
+fpath+=( ${ANTIBODY_HOME}/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure )
+source ${ANTIBODY_HOME}/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+fpath+=( ${ANTIBODY_HOME}/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting )
+source ${ANTIBODY_HOME}/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
+fpath+=( ${ANTIBODY_HOME}/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
+source ${ANTIBODY_HOME}/https-COLON--SLASH--SLASH-github.com-SLASH-jessestuart-SLASH-gtm-terminal-plugin/gtm-plugin.sh
+fpath+=( ${ANTIBODY_HOME}/https-COLON--SLASH--SLASH-github.com-SLASH-jessestuart-SLASH-gtm-terminal-plugin )
+source ${ANTIBODY_HOME}/https-COLON--SLASH--SLASH-github.com-SLASH-sobolevn-SLASH-wakatime-zsh-plugin/wakatime.plugin.zsh
+fpath+=( ${ANTIBODY_HOME}/https-COLON--SLASH--SLASH-github.com-SLASH-sobolevn-SLASH-wakatime-zsh-plugin )
